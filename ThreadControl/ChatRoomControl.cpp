00359a77196f90403e43efb248ca08607ad64c4a
NUTT1101 <zhangf099@gmail.com>
2022-01-04 23:52:24 +0800

    send packet cut

--- ThreadControl/ChatRoomControl.cpp
+++ ThreadControl/ChatRoomControl.cpp
@@ -1,7 +1,7 @@
 #include "ChatRoomThread.hpp"
 #include <string>
 #include <sstream>
-#include <ctime>
+#include <QMap>
 
 ChatRoomThread::ChatRoomThread(QListWidget *view) {
     this->view = view;
@@ -14,16 +14,64 @@ void ChatRoomThread::run() {
     const u_char *packet;
     int resource;
     
+    QMap<int, std::string> undonePackets;
     while ((resource = pcap_next_ex(global_openedInterface, &packet_header, &packet)) >= 0) {
         if (global_openedInterface == nullptr) break;
         if (resource == 0) continue;
         
-        if (packet[12] != 0xff && packet[13] != 0x01) continue;
+        if (packet[12] != 0xff && packet[13] != 0x0e) continue;
         
         std::string message = "";
-        int packet_length = packet[14] * 255 + packet[15];
         
-        for (int i=16; i < packet_length + 16; i++) {
+        /* int colonIndex;
+        for (int i=0; i < packet_header->caplen; i++) {
+            if (packet[i] == 0xbc && packet[i+1] == 0x9a && packet[i+2] == 0x20) {
+                colonIndex = i+2;
+                break;
+            }
+        }
+
+        std::string sender;
+        for (int i=20; i <= colonIndex; i++) {
+            std::stringstream byte;
+
+            byte << (char) ((int) packet[i]);
+            sender += byte.str();
+        } 
+        
+        int packetIsComplete = packet[18] * 255 + packet[19];
+        int packetNumber = packet[16] * 255 + packet[17];
+        
+        if (packetIsComplete > 0) {
+            if (!undonePackets.contains(packetNumber)) {
+                for (int i=colonIndex + 1; i < packet_header->caplen; i++) {
+                    std::stringstream byte;
+
+                    byte << (char) ((int) packet[i]);
+                    message += byte.str();
+                }
+
+                this->view->addItem(QString::fromStdString(sender + message));
+                undonePackets[packetNumber] += sender + message;
+            } else {
+                for (int i=colonIndex + 1; i < packet_header->caplen; i++) {
+                    std::stringstream byte;
+
+                    byte << (char) ((int) packet[i]);
+                    message += byte.str();
+                }
+                this->view->addItem(QString::fromStdString(message));
+                undonePackets[packetNumber] += message;
+            }
+
+            if (packetIsComplete == 1) {
+                undonePackets.remove(undonePackets.key(undonePackets[packetNumber]));
+            }
+
+            continue;
+        } */
+        
+        for (int i=20; i < packet_header->caplen; i++) {
             std::stringstream byte;
 
             byte << (char) ((int) packet[i]);
--- event/SendButtonClickEvent.cpp
+++ event/SendButtonClickEvent.cpp
@@ -1,6 +1,5 @@
 #include "./SendButtonClick/SendButtonClick.hpp"
 #include "pcap.h"
-#include <ctime>
 
 SendButtonClick::SendButtonClick() {}
 SendButtonClick::~SendButtonClick() {}
@@ -14,45 +13,70 @@ void SendButtonClick::sendMessage() {
             return;
         }
 
-        if (message.size() > 1584) {
+        /* if (message.size() > 1584) {
             QMessageBox::warning(mainWindow, "warning", 
                 "<h3>發送的文字上限不能超過一千五個字</h3>", QMessageBox::Ok);
             return;
-        }
+        } */
 
         u_char packet[1500] = {'\0'};
 
         for (int i=0; i < 6; i++) {
 			packet[i] = 0xff;
 		}
-
-        packet[6] = 0xaa;
-        packet[7] = 0xbb;
-        packet[8] = 0xcc;
-        packet[9] = 0xdd;
-        packet[10] = 0xee;
-        packet[11] = 0xff;
+        
+        for (int i=6; i < 12; i++) {
+            packet[i] = i + 4;
+        }
 		
         packet[12] = 0xff;
-		packet[13] = 1; // 組別編號
+		packet[13] = 14; // 組別編號
 		
 		// 把輸入的訊息裝進封包內
         message = userName + "： " + message;
+        int userNameUtf8Length = (userName + "： ").toUtf8().size();
 
-        QByteArray message_utf8 = message.toUtf8();
+        QByteArray messageUft8 = message.toUtf8();
 
-        int message_length = message_utf8.size();
+        int messageLength = messageUft8.size();
 
-        packet[14] = message_length / 255;
-        packet[15] = message_length % 255;
+        packet[14] = messageLength / 255;
+        packet[15] = messageLength % 255;
+        packet[16] = number / 255;
+        packet[17] = number % 255;
+
+        int packetTotalLength = messageLength + 20 + userNameUtf8Length;
+        chatRoom->getChatRoom()->addItem(QVariant(packetTotalLength).toString());
         
-		for (int i = 16; i < message_length + 16; i++) {
-			packet[i] = (char) message_utf8.at(i - 16);
-		}
+        if (packetTotalLength > 1500) {
+            int sendTimes = messageLength / 1500 + (messageLength % 1500 > 0 ? 1 : 0); 
+            packet[18] = sendTimes / 255;
+            packet[19] = sendTimes % 255;
 
-        pcap_sendpacket(global_openedInterface, packet, message_length + 16);
+            while (packetTotalLength > 0) {
+                for (int i = 20; i < (packetTotalLength > 1500 ? 1500 : packetTotalLength); i++) {
+                    packet[i] = (char) messageUft8.at(i - 20);
+                }
+                pcap_sendpacket(global_openedInterface, packet,
+                    packetTotalLength > 1500 ? 1500 : packetTotalLength);
+                int ruduceOneFromSendTimes = packet[18] * 255 + packet[19] - 1;  
+                
+                packet[18] = ruduceOneFromSendTimes / 255;
+                packet[19] = ruduceOneFromSendTimes % 255;
+                
+                packetTotalLength -= 1480;
+            }
+
+        } else {
+            for (int i = 20; i < packetTotalLength; i++) {
+                packet[i] = (char) messageUft8.at(i - 20);
+            }
+
+            pcap_sendpacket(global_openedInterface, packet, packetTotalLength);
+        }
 
         sendBar->getMessageLine()->setText("");
         chatRoom->getChatRoom()->scrollToBottom();
+        number++;
     }
 }
